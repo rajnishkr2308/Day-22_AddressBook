d8e321d2851befab2b3a608c001e90ddaf377e6d
rajnishkr2308 <raj123rajnish2308@gmail.com>
2023-05-12 23:46:24 +0530

    Added:Each Address Book has unique name

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -50,7 +50,7 @@ public class AddressBook {
 
     public void editDetails(){
         System.out.println("Enter the Name of the Contact you want to Edit:");
-        String name= in.nextLine();
+        String name= in.next();
         boolean contactFound=false;
 
         for (Contact Temp : contacts) {
@@ -107,7 +107,7 @@ public class AddressBook {
 
     public void deleteDetails(){
         System.out.println("Enter the Name of the Contact you want to Delete:");
-        String name= in.nextLine();
+        String name= in.next();
         boolean contactFound=false;
 
         for (Contact Temp : contacts) {
